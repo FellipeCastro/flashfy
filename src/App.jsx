45f2ee495c50a62a00c2320b7e4937dc6a65ee31
Fellipe Castro <fellipecastro27d@gmail.com>
2025-06-17 20:35:06 -0300

    Projeto iniciado

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,180 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <div className="container open">
+            <div className="sidebar">
+                <div className="flexContainer">
+                    <button className="sidebarBtn">
+                        <MdKeyboardDoubleArrowRight />
+                    </button>
+                    <div className="logo">FlashFy</div>
+                </div>
+            </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+            <div className="mainContainer">
+                <div className="titleContainer">
+                    <h1>Meus decks</h1>
+                    <div className="btnsContainer">
+                        <button className="btn secondary">+ Novo deck</button>
+                        <button className="btn">+ Novo card</button>
+                    </div>
+                </div>
+                <div className="filterContainer">
+                    <button
+                        style={{ backgroundColor: "hsla(200, 75%, 50%, 0.4)" }}
+                    >
+                        Matemática
+                    </button>
+                    <button
+                        style={{ backgroundColor: "hsla(20, 85%, 55%, 0.4)" }}
+                    >
+                        História
+                    </button>
+                    <button
+                        style={{ backgroundColor: "hsla(120, 60%, 45%, 0.4)" }}
+                    >
+                        Ciência
+                    </button>
+                    <button
+                        style={{ backgroundColor: "hsla(200, 75%, 50%, 0.4)" }}
+                    >
+                        Matemática
+                    </button>
+                    <button
+                        style={{ backgroundColor: "hsla(20, 85%, 55%, 0.4)" }}
+                    >
+                        História
+                    </button>
+                    <button
+                        style={{ backgroundColor: "hsla(120, 60%, 45%, 0.4)" }}
+                    >
+                        Ciência
+                    </button>
+                    <button
+                        style={{ backgroundColor: "hsla(200, 75%, 50%, 0.4)" }}
+                    >
+                        Matemática
+                    </button>
+                    <button
+                        style={{ backgroundColor: "hsla(20, 85%, 55%, 0.4)" }}
+                    >
+                        História
+                    </button>
+                    <button
+                        style={{ backgroundColor: "hsla(120, 60%, 45%, 0.4)" }}
+                    >
+                        Ciência
+                    </button>
+                    <button
+                        style={{ backgroundColor: "hsla(200, 75%, 50%, 0.4)" }}
+                    >
+                        Matemática
+                    </button>
+                    <button
+                        style={{ backgroundColor: "hsla(20, 85%, 55%, 0.4)" }}
+                    >
+                        História
+                    </button>
+                    <button
+                        style={{ backgroundColor: "hsla(120, 60%, 45%, 0.4)" }}
+                    >
+                        Ciência
+                    </button>
+                </div>
+                <ul className="decksConatiner">
+                    <li className="deck">
+                        <strong
+                            style={{
+                                backgroundColor: "hsla(200, 75%, 50%, 0.4)",
+                            }}
+                        >
+                            Matemática
+                        </strong>
+                        <h3>Funções quadráticas</h3>
+                        <p>6 cards</p>
+                    </li>
+                    <li className="deck">
+                        <strong
+                            style={{
+                                backgroundColor: "hsla(120, 60%, 45%, 0.4)",
+                            }}
+                        >
+                            Ciências
+                        </strong>
+                        <h3>Funções quadráticas</h3>
+                        <p>6 cards</p>
+                    </li>
+                    <li className="deck">
+                        <strong
+                            style={{
+                                backgroundColor: "hsla(200, 75%, 50%, 0.4)",
+                            }}
+                        >
+                            Matemática
+                        </strong>
+                        <h3>Expressões numéricas</h3>
+                        <p>6 cards</p>
+                    </li>
+                    <li className="deck">
+                        <strong
+                            style={{
+                                backgroundColor: "hsla(20, 85%, 55%, 0.4)",
+                            }}
+                        >
+                            História
+                        </strong>
+                        <h3>Funções quadráticas</h3>
+                        <p>6 cards</p>
+                    </li>
+                    <li className="deck">
+                        <strong
+                            style={{
+                                backgroundColor: "hsla(200, 75%, 50%, 0.4)",
+                            }}
+                        >
+                            Matemática
+                        </strong>
+                        <h3>Funções quadráticas</h3>
+                        <p>6 cards</p>
+                    </li>
+                    <li className="deck">
+                        <strong
+                            style={{
+                                backgroundColor: "hsla(20, 85%, 55%, 0.4)",
+                            }}
+                        >
+                            História
+                        </strong>
+                        <h3>Funções quadráticas</h3>
+                        <p>6 cards</p>
+                    </li>
+                    <li className="deck">
+                        <strong
+                            style={{
+                                backgroundColor: "hsla(120, 60%, 45%, 0.4)",
+                            }}
+                        >
+                            Ciências
+                        </strong>
+                        <h3>Funções quadráticas</h3>
+                        <p>6 cards</p>
+                    </li>
+                    <li className="deck">
+                        <strong
+                            style={{
+                                backgroundColor: "hsla(200, 75%, 50%, 0.4)",
+                            }}
+                        >
+                            Matemática
+                        </strong>
+                        <h3>Expressões numéricas</h3>
+                        <p>6 cards</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
 
-export default App
+export default App;
